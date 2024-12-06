b4ec1103d7b1b9c67f5fae4f52f1a6094d65d7bb
Nimesh8848 <nimeshmathema@gmail.com>
2024-12-06 08:55:29 +0545

    Select statement

---
+++ 2.select statement.sql
@@ -0,0 +1,5 @@
+#Select * from parks_and_recreation.employee_demographics
+select first_name from parks_and_recreation.employee_demographics;
+select distinct gender from parks_and_recreation.employee_demographics;
+
+select age,age+10 from parks_and_recreation.employee_demographics;
