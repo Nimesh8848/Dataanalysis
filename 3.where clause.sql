736c4e1eac605c904c17b4096fbad26d5e1d0636
Nimesh8848 <nimeshmathema@gmail.com>
2024-12-06 09:02:40 +0545

    where clause

---
+++ 3.where clause.sql
@@ -0,0 +1,4 @@
+SELECT * FROM employee_salary where first_name ='Leslie';
+select * from employee_salary where salary<50000;
+select * from employee_demographics where gender= 'male' and birth_date>='1985-07-26';
+select *from employee_demographics where(first_name ='Tom' and age=36)or age<36;
