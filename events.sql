7181a46eedd243a75d8b66e3f0eab22368b85f17
Nimesh8848 <nimeshmathema@gmail.com>
2024-12-08 21:31:40 +0545

    events

---
+++ events.sql
@@ -0,0 +1,11 @@
+Delimiter $$
+create event delete_retires
+on schedule every 30 second
+do
+begin
+	delete
+    from employee_demographics
+    where age>=60;
+end $$
+delimiter ;
+select * from employee_demographics;
